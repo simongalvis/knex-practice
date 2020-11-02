d0c668cae5535c9942a741abe584ac259887fe5a
Simon Galvis <simongalvis9@gmail.com>
2020-11-02 13:27:04 -0500

    Add and populate temp.spec.js

--- test/articles-service.spec.js
+++ test/articles-service.spec.js
@@ -1,6 +1,15 @@
 const ArticleService = require('../src/articles-service.js')
+const knex = require('knex')
 
 describe(`Articles service object`, function(){
+  let db
+
+  before(() =>{
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DB_URL,
+    })
+  })
   describe(`getAllArticles()`, () =>{
     it(`resolves all articles from 'blogful_articles' table`, () =>{
 
