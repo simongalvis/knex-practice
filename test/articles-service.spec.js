84fac35bf6ece721e822613aa8021ad6bf7193dd
Simon Galvis <simongalvis9@gmail.com>
2020-11-02 12:34:58 -0500

    Add test file and test db

---
+++ test/articles-service.spec.js
@@ -0,0 +1,9 @@
+const ArticleService = require('../src/articles-service.js')
+
+describe(`Articles service object`, function(){
+  describe(`getAllArticles()`, () =>{
+    it(`resolves all articles from 'blogful_articles' table`, () =>{
+
+    })
+  })
+})
